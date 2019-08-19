c4086aff6724e76b39111af79b1911c35a148989
Forl <kumaloveslife@gmail.com>
2019-08-19 23:53:23 +0800

    完成一个简单的九宫格五子棋

---
+++ demo1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      hi im app
+    </div>
+  );
+}
+
+export default App;
---
+++ demo1/src/demo1.js
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+
+const Header = (
+  <header>
+    header
+  </header>
+)
+const Header2 = function(props) {
+  return (
+    <header>
+      header{props.name}
+    </header>
+  )
+}
+const Button = (
+  <div>
+    button
+  </div>
+)
+
+console.log(Button)
+
+const Button2 = function() {
+  const [n, setN] = React.useState(0)
+  return (
+    <div>
+      {n}
+      <button onClick={function(){
+        setN(n+1)
+      }}>click +1</button>
+    </div>
+  )
+}
+class Button3 extends React.Component{
+  render(){
+    return (
+      <div>bottom3</div>
+    )
+  }
+}
+const div = (
+  <div>
+    {Header}
+    {Header2({name: 'jack'})}
+    <Header2 name='kuma' />
+    <p>
+      <span>hello</span>
+    </p>
+    {Button}
+    <Button2/>
+    <Button3/>
+    <App/>
+  </div>
+);
+
+console.log(div);
+
+ReactDOM.render(div, document.getElementById("root"));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
