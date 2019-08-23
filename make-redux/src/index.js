bbb1d91038775e5c0264bc8ade2fb5d0093d3ab3
Forl <kumaloveslife@gmail.com>
2019-08-23 15:10:49 +0800

    简单限制修改公共数据

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -2,11 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appState = {
+  title: {
+    text: 'React.js redux',
+    color: 'red'
+  },
+  content: {
+    text: 'React.js content',
+    color: 'blue'
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function renderApp(appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle(title) {
+  const titleDom = document.getElementById('title')
+  titleDom.innerHTML = title.text
+  titleDom.style.color = title.color
+}
+
+function renderContent(content) {
+  const contentDom = document.getElementById('content')
+  contentDom.innerHTML = content.text
+  contentDom.style.color = content.color
+}
+
+//专门负责修改
+function dispatch (action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+dispatch({type: 'UPDATE_TITLE_TEXT', text: '修改title text'})
+
+renderApp(appState)
