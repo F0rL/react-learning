0660406b3cf3610cae99aa2dd48d5fd7fcd9fc64
Forl <kumaloveslife@gmail.com>
2019-08-24 00:05:16 +0800

    优化themeSwitch

--- make-redux/src/react-redux.js
+++ make-redux/src/react-redux.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export const connect = mapStateToProps => WrappendComponent => {
+export const connect = (mapStateToProps, mapDispatchToProps) => WrappendComponent => {
   class Connect extends Component {
     static contextTypes = {
       store: PropTypes.object
     };
-    
+
     constructor(){
       super()
       this.state = {allProps: {}}
@@ -20,18 +20,22 @@ export const connect = mapStateToProps => WrappendComponent => {
 
     _updateProps () {
       const { store } = this.context
-      let stateProps = mapStateToProps(store.getState(), this.props) // 额外传入 props，让获取数据更加灵活方便
+      let stateProps = mapStateToProps
+        ? mapStateToProps(store.getState(), this.props)
+        : {} // 防止 mapStateToProps 没有传入
+      let dispatchProps = mapDispatchToProps
+        ? mapDispatchToProps(store.dispatch, this.props)
+        : {} // 防止 mapDispatchToProps 没有传入
       this.setState({
-        allProps: { // 整合普通的 props 和从 state 生成的 props
+        allProps: {
           ...stateProps,
+          ...dispatchProps,
           ...this.props
         }
       })
     }
     render() {
-      const { store } = this.context;
-      let stateProps = mapStateToProps(store.getState());
-      return <WrappendComponent {...stateProps} />;
+      return <WrappendComponent {...this.state.allProps}/>;
     }
   }
   return Connect;
