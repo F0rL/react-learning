84d8951ffff68c0f8e6ec544b4e996f84e8555bb
Forl <kumaloveslife@gmail.com>
2019-08-29 00:27:20 +0800

    prop-types 添加校验

--- todolist/src/TodoItem.js
+++ todolist/src/TodoItem.js
@@ -1,24 +1,38 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.handleDelete = this.handleDelete.bind(this);
   }
-  handleDelete(){
-    const { deleteItem, index } = this.props
-    deleteItem(index)
+  handleDelete() {
+    const { deleteItem, index } = this.props;
+    deleteItem(index);
   }
   render() {
-    const { time, content } = this.props.item
+    const { time, content } = this.props.item;
+    const { test } = this.props
     return (
       <li className="todo_item">
-        <span>时间: {time}</span>
+        <span>{test}: {time}</span>
         <span>内容: {content}</span>
         <span onClick={this.handleDelete}>X</span>
       </li>
     );
   }
+}
+
+TodoItem.propTypes = {
+  item: PropTypes.object.isRequired,
+  deleteItem: PropTypes.func,
+  index: PropTypes.number,
+  test: PropTypes.string.isRequired
+};
+
+//默认值
+TodoItem.defaultProps = {
+  test: '年月日'
 }
 
 export default TodoItem;
